5bd74f58438be4a3a9da4c7a8a15a187b32e7739
YammyShep <jdbuelow@gmail.com>
2024-06-03 16:14:39 -0500

    Use VaultAPI to store trusted status in permissions plugin

--- src/main/java/com/jbuelow/servercore/ServerCore.java
+++ src/main/java/com/jbuelow/servercore/ServerCore.java
@@ -5,6 +5,8 @@ import com.jbuelow.servercore.trust.TrustCheckEventListener;
 import com.jbuelow.servercore.trust.UntrustCommand;
 import com.jbuelow.servercore.trust.service.MapUserTrustService;
 import com.jbuelow.servercore.trust.service.UserTrustService;
+import com.jbuelow.servercore.trust.service.VaultPermissionsUserTrustService;
+import net.milkbowl.vault.permission.Permission;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.ServicePriority;
@@ -30,7 +32,10 @@ public final class ServerCore extends JavaPlugin {
     public static UserTrustService getTrustService() {
         RegisteredServiceProvider<UserTrustService> provider = Bukkit.getServicesManager().getRegistration(UserTrustService.class);
         if (provider == null) {
-            Bukkit.getServicesManager().register(UserTrustService.class, new MapUserTrustService(), getPlugin(ServerCore.class), ServicePriority.Normal);
+            RegisteredServiceProvider<Permission> rsp = Bukkit.getServicesManager().getRegistration(Permission.class);
+            Permission vaultPerms = rsp.getProvider();
+
+            Bukkit.getServicesManager().register(UserTrustService.class, new VaultPermissionsUserTrustService(vaultPerms), getPlugin(ServerCore.class), ServicePriority.Normal);
 
             provider = Bukkit.getServicesManager().getRegistration(UserTrustService.class);
 
---
+++ src/main/java/com/jbuelow/servercore/trust/service/VaultPermissionsUserTrustService.java
@@ -0,0 +1,39 @@
+package com.jbuelow.servercore.trust.service;
+
+import net.milkbowl.vault.permission.Permission;
+import org.bukkit.entity.Player;
+
+public class VaultPermissionsUserTrustService implements UserTrustService {
+
+    private static final String TRUSTED_PERM = "servercore.trust.trusted";
+
+    private final Permission perms;
+
+    public VaultPermissionsUserTrustService(Permission perms) {
+        this.perms = perms;
+    }
+
+    @Override
+    public boolean isTrusted(Player player) {
+        return perms.has(player, TRUSTED_PERM);
+    }
+
+    @Override
+    public void setTrust(Player player, boolean isTrusted) {
+        if (isTrusted) {
+            perms.playerAdd(null, player, TRUSTED_PERM);
+        } else {
+            perms.playerRemove(null, player, TRUSTED_PERM);
+        }
+    }
+
+    @Override
+    public void trustPlayer(Player player) {
+        setTrust(player, true);
+    }
+
+    @Override
+    public void revokeTrust(Player player) {
+        setTrust(player, false);
+    }
+}
