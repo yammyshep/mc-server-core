3833a43013cbdf039a041476e291c6711256bf50
YammyShep <jdbuelow@gmail.com>
2025-01-15 15:10:28 -0600

    Make max distance and preventing void damage in spectator mode configurable

--- src/main/java/com/jbuelow/servercore/spectator/event/SpectatorEventListener.java
+++ src/main/java/com/jbuelow/servercore/spectator/event/SpectatorEventListener.java
@@ -1,7 +1,9 @@
 package com.jbuelow.servercore.spectator.event;
 
+import com.jbuelow.servercore.ServerCore;
 import com.jbuelow.servercore.spectator.SpectatorManager;
 import com.jbuelow.servercore.spectator.SpectatorModule;
+import dev.dejvokep.boostedyaml.YamlDocument;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,18 +13,30 @@ import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.util.Vector;
 
 public class SpectatorEventListener implements Listener {
-    private final SpectatorModule module;
     private final SpectatorManager manager;
-    private double maxDistance = 100;
-    private double maxDistanceSquared = maxDistance * maxDistance; //TODO: Make configuration value
+
+    private final boolean preventVoidDamage;
+
+    private final double maxDistance;
+    private final double maxDistanceSquared;
 
     public SpectatorEventListener(SpectatorModule module) {
-        this.module = module;
         manager = module.getManager();
+        YamlDocument config = ServerCore.get().getConfiguration();
+
+        preventVoidDamage = config.getBoolean("spectator.prevent-void-damage", true);
+        maxDistance = config.getDouble("spectator.max-distance", Double.POSITIVE_INFINITY);
+        maxDistanceSquared = maxDistance * maxDistance;
     }
 
     @EventHandler(ignoreCancelled = true)
     public void onEntityDamage(EntityDamageEvent event) {
+        if (!preventVoidDamage) {
+            event.getHandlers().unregister(this);
+            return;
+        }
+
+        // Prevent players in spectator mode from taking void damage
         if (event.getCause() != EntityDamageEvent.DamageCause.VOID) {
             return;
         }
@@ -36,10 +50,16 @@ public class SpectatorEventListener implements Listener {
 
     @EventHandler(ignoreCancelled = true)
     public void onPlayerMove(PlayerMoveEvent event) {
+        if (maxDistance == Double.POSITIVE_INFINITY || maxDistance < 0.0) {
+            event.getHandlers().unregister(this);
+            return;
+        }
+
         if (!manager.isPlayerSpectating(event.getPlayer())) {
             return;
         }
 
+        // Prevent players in spectator mode from moving too far away from their non-spectator position
         Location returnLocation = manager.getReturnLocation(event.getPlayer());
         double distance = event.getTo().distanceSquared(returnLocation);
         if (distance >= maxDistanceSquared) {
