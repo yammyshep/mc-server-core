899a94224d3adaff83f8058fbc4acc2daeeb0348
YammyShep <jdbuelow@gmail.com>
2025-01-19 14:22:59 -0600

    Prevent balloon from clearing levitation from shulker damage

--- src/main/java/com/jbuelow/servercore/balloon/BalloonEventListener.java
+++ src/main/java/com/jbuelow/servercore/balloon/BalloonEventListener.java
@@ -2,19 +2,30 @@ package com.jbuelow.servercore.balloon;
 
 import com.jbuelow.servercore.ServerCore;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.entity.EntityPotionEffectEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.*;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.util.BoundingBox;
+import org.bukkit.util.Vector;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class BalloonEventListener implements Listener {
     private static final BalloonItem balloonItem = new BalloonItem();
+
+    private final Map<Player, PotionEffect> prevLevitationEffects = new HashMap<>();
 
     private boolean isBalloon(ItemStack itemStack) {
         if (itemStack == null) {
@@ -35,14 +46,32 @@ public class BalloonEventListener implements Listener {
         }
     }
 
-    //TODO: Don't rely on potion effects! Creates ability to remove the LEVITATION potion effect using the balloon item!
-    // also grants the "Great view from up here" challange prematurely
+    public boolean isBallooning(Player player) {
+        return prevLevitationEffects.containsKey(player);
+    }
+
     private void startBalloon(Player player) {
+        if (isBallooning(player)) {
+            return;
+        }
+
+        prevLevitationEffects.put(player, player.getPotionEffect(PotionEffectType.LEVITATION));
+        player.removePotionEffect(PotionEffectType.LEVITATION);
+
         player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, -1, 1, true));
     }
 
     private void endBalloon(Player player) {
+        if (!isBallooning(player)) {
+            return;
+        }
+
         player.removePotionEffect(PotionEffectType.LEVITATION);
+
+        PotionEffect restoredLevitation = prevLevitationEffects.remove(player);
+        if (restoredLevitation != null) {
+            player.addPotionEffect(restoredLevitation);
+        }
     }
 
     // Called when the player changes what item is selected in the hotbar
@@ -99,6 +128,31 @@ public class BalloonEventListener implements Listener {
             Bukkit.getScheduler().runTaskLater(ServerCore.get(), () -> {
                 updatePlayerBalloonState((Player) event.getWhoClicked());
             }, 1L);
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onEntityPotionEffect(EntityPotionEffectEvent event) {
+        if (!(event.getEntity() instanceof Player targetPlayer)) {
+            return;
+        }
+
+        if (!isBallooning(targetPlayer)) {
+            return;
+        }
+
+        if (event.getModifiedType() != PotionEffectType.LEVITATION) {
+            return;
+        }
+
+        if (event.getCause() == EntityPotionEffectEvent.Cause.PLUGIN) {
+            return;
+        }
+
+        if (event.getAction() == EntityPotionEffectEvent.Action.ADDED ||
+                event.getAction() == EntityPotionEffectEvent.Action.CHANGED) {
+            event.setCancelled(true);
+            prevLevitationEffects.put(targetPlayer, event.getNewEffect());
         }
     }
 }
